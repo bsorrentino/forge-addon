3533fcfacdc4e171a399a351002c00a21a2cd648
bsorrentino <bartolomeo.sorrentino@gmail.com>
2014-05-30 09:35:23 +0200

    add new scripts

--- dynjs-addon/src/test/resources/introspection.js
+++ dynjs-addon/src/test/resources/introspection.js
@@ -1,24 +1,34 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 module.exports = {
-   
+
     printC:function( name, o ) {
         if( o ) {
             print( name + ": " + o.class.name );
         }
     },
     printM:function( name, o ) {
-        if( o ) { 
+        if( o ) {
             var methods = o.class.declaredMethods;
             //var methods = o.getClass().getMethods();
             print( "Method of " +  name);
             for( var i = 0; i< methods.length; ++i ) {
                 print( "\t" + methods[i].name );
-            }  
+            }
+        }
+    },
+    printSM:function( name, o ) {
+        if( o ) {
+            var methods = o.declaredMethods;
+            //var methods = o.getClass().getMethods();
+            print( "Method of " +  name);
+            for( var i = 0; i< methods.length; ++i ) {
+                print( "\t" + methods[i].name );
+            }
         }
     }
 };
