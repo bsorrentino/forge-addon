2fa0c23c047a3c760b36850e14fa2f06e025d206
bsorrentino <bartolomeo.sorrentino@gmail.com>
2014-08-01 19:57:10 +0200

    update wizard

--- dynjs/addon.js
+++ dynjs/addon.js
@@ -4,28 +4,56 @@
 
 print( "addon loaded!");
 
+var attrs = {
+	
+	ic:null
+};
+
+function initializeUI( builder ) {
+
+	print( "initialize UI")
+
+	attrs.ic = self.componentFactory.createInput("test", "t", java.lang.String);
+	attrs.ic.setLabel( "Test");
+	attrs.ic.setRequired( true );
+	//attrs.ic.setDefaultValue( "OK");
+	attrs.ic.setValue("X");
+	builder.add( attrs.ic );
+
+	print( "UI initialized!")
+}
+
+function execute( context ) {
+
+	print( "executeJS " + context.getUIContext());
+	
+	return "OK " +  attrs.ic.getValue();
+}
+
+/*
 {
 		
-ic:null
+ic:null,
 
 
 initializeUI:function( builder ) {
 
 	print( "initialize UI")
 
-	attrs.ic = self.componentFactory.createInput("test", "test", java.lang.String);
-	attrs.ic.setLabel( "Test:");
-	attrs.ic.setRequired( true );
+	this.ic = self.componentFactory.createInput("test", "test", java.lang.String);
+	this..ic.setLabel( "Test:");
+	this.ic.setRequired( true );
 	builder.add( attrs.ic );
 
 	print( "UI initialized!")
-}
+},
 
 execute:function( context ) {
 
 	print( "executeJS " + context.getUIContext());
-
-	var attrs = context.getUIContext().getAttributeMap();
 	
 	return "OK " +  attrs.ic;
 }
+
+}
+*/
